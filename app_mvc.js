3d86b80eae5bdbc9a82fc50d2b5a5b928d6d6fb1
a.istibayev <a.istibayev@sergek.kz>
2021-11-04 17:18:07 +0600

    04_11_21 commit

--- app_mvc.js
+++ app_mvc.js
@@ -18,38 +18,39 @@ app.use(cors({
 }));
 
 app.get('/', require('./cont/cont_list'));
+app.get('/mainPage', require('./cont/mainPage'));
 app.get('/moviePage1', require('./cont/moviePage1'));
 app.get('/moviePage2', require('./cont/moviePage2'));
-app.get('/seriesPage1', require('./cont/seriesPage1'));
-app.get('/seriesPage2', require('./cont/seriesPage2'));
+app.get('/tvSeriesPage1', require('./cont/seriesPage1'));
+app.get('/tvSeriesPage2', require('./cont/seriesPage2'));
 
 //moviesElement page1
-app.get('/Hamilton', require('./moviesElement/Hamilton'));
-app.get('/Lives_dogs', require('./moviesElement/Lives_dogs'));
-app.get('/The_shawshank_redemption', require('./moviesElement/The_Shawshank_Redemption'));
-app.get('/the_green_mile', require('./moviesElement/The_Green_Mile'));
-app.get('/not_dimon', require('./moviesElement/not_dimon'));
+app.get('/movie/Hamilton', require('./moviesElement/Hamilton'));
+app.get('/movie/Lives_dogs', require('./moviesElement/Lives_dogs'));
+app.get('/movie/The_shawshank_redemption', require('./moviesElement/The_Shawshank_Redemption'));
+app.get('/movie/the_green_mile', require('./moviesElement/The_Green_Mile'));
+app.get('/movie/not_dimon', require('./moviesElement/not_dimon'));
 
 // //moviesElement page2
-app.get('/Smeshariki', require('./moviesElement/Smeshariki'));
-app.get('/Forrest_gump', require('./moviesElement/Forrest_gump'));
-app.get('/Armin', require('./moviesElement/Armin'));
-app.get('/queen', require('./moviesElement/queen'));
-app.get('/twelfth', require('./moviesElement/twelfth'));
+app.get('/movie/Smeshariki', require('./moviesElement/Smeshariki'));
+app.get('/movie/Forrest_gump', require('./moviesElement/Forrest_gump'));
+app.get('/movie/Armin', require('./moviesElement/Armin'));
+app.get('/movie/queen', require('./moviesElement/queen'));
+app.get('/movie/twelfth', require('./moviesElement/twelfth'));
 
 // //tvSeries page1
-app.get('/friends', require('./tvSeries/friends'));
-app.get('/people', require('./tvSeries/people'));
-app.get('/badComedian', require('./tvSeries/badComedian'));
-app.get('/planet', require('./tvSeries/planet'));
-app.get('/blue', require('./tvSeries/blue'));
+app.get('/tvSeries/friends', require('./tvSeries/friends'));
+app.get('/tvSeries/people', require('./tvSeries/people'));
+app.get('/tvSeries/badComedian', require('./tvSeries/badComedian'));
+app.get('/tvSeries/planet', require('./tvSeries/planet'));
+app.get('/tvSeries/blue', require('./tvSeries/blue'));
 
 // //tvSeries page2
-app.get('/thrones', require('./tvSeries/thrones'));
-app.get('/chernobyl', require('./tvSeries/chernobyl'));
-app.get('/romanos', require('./tvSeries/romanos'));
-app.get('/space', require('./tvSeries/space'));
-app.get('/seven', require('./tvSeries/seven'));
+app.get('/tvSeries/thrones', require('./tvSeries/thrones'));
+app.get('/tvSeries/chernobyl', require('./tvSeries/chernobyl'));
+app.get('/tvSeries/romanos', require('./tvSeries/romanos'));
+app.get('/tvSeries/space', require('./tvSeries/space'));
+app.get('/tvSeries/seven', require('./tvSeries/seven'));
 
 
 
--- tvSeries/friends.js
+++ tvSeries/friends.js
@@ -3,7 +3,7 @@ var task = require('../model/model_task');
 module.exports =
     function (req, res) {
         res.send(
-            { "id": "1399", "id_kinopoisk": "77044", "url": "265736-druzya-1994", "type": "tv-series", "title": "Друзья", "title_alternative": "Friends", "tagline": "«Everybody needs Friends!»", "description": "Главные герои Рейчел, Моника, Фиби, Джоуи, Чендлер и Росс. Три девушки и три парня, которые дружат, живут по соседству, вместе убивают время и противостоят жестокой реальности, делятся своими секретами и иногда очень сильно влюбляются.", "year": 1994, "poster": "images.kinopoisk.cloud\/posters\/77044.jpg" }
+            { "id": "1399", "id_kinopoisk": "77044", "url": "265736-druzya-1994", "type": "tv-series", "title": "Друзья", "title_alternative": "Friends", "tagline": "«Everybody needs Friends!»", "description": "Главные герои Рейчел, Моника, Фиби, Джоуи, Чендлер и Росс. Три девушки и три парня, которые дружат, живут по соседству, вместе убивают время и противостоят жестокой реальности, делятся своими секретами и иногда очень сильно влюбляются.", "year": 1994, "poster": "\/\/images.kinopoisk.cloud\/posters\/77044.jpg" }
         );
     };
 
